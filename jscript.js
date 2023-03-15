4f715351b5f004490c267fe0986c609bb766a2c3
ezamusyarof <ezamusyarof06@gmail.com>
2023-03-15 22:03:35 +0700

    first commit

---
+++ jscript.js
@@ -0,0 +1,15 @@
+function menu_arsip(){
+    window.location.href = "arsip.html";
+}
+function menu_call(){
+    window.location.href = "call.html";
+}
+function menu_status(){
+    window.location.href = "status.html";
+}
+function menu_contact(){
+    window.location.href = "contact.html";
+}
+function menu_chat(){
+    window.location.href = "index.html";
+}
